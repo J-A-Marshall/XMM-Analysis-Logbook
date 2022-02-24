c28131f00854b77b2632f9c1c7e68f29ab018143
J-A-Marshall <jamieamarshall1678@gmail.com>
2022-02-24 19:41:50 +0000

    added new revs

---
+++ myjupbook/src/_build/jupyter_execute/rev_2409.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+from IPython.display import Image
+
+
+# # Revolution 2409
+
+# #### PN
+
+# In[2]:
+
+
+Image(filename="2409_figs/pn_image.png")
+
+
+# In[3]:
+
+
+Image(filename="2409_figs/pn_lc.png")
+
+
+# In[4]:
+
+
+Image(filename="2409_figs/pn_pileup.png")
+
+
+# #### MOS
+
+# ##### MOS1
+
+# In[5]:
+
+
+Image(filename="2409_figs/mos1_lc.png")
+
+
+# In[7]:
+
+
+Image(filename="2409_figs/mos1_pileup.png")
+
+
+# ##### MOS2
+
+# In[9]:
+
+
+Image(filename="2409_figs/mos2_lc.png")
+
+
+# In[10]:
+
+
+Image(filename="2409_figs/mos2_pileup.png")
+
+
+# 
