73d87b5eff7a4dc77b374f3382b49e89d5eaab21
J-A-Marshall <jamieamarshall1678@gmail.com>
2022-03-10 14:29:06 +0000

    added final figs

---
+++ myjupbook/src/_build/jupyter_execute/Final_Spectra.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+from IPython.display import Image
+
+
+# # Fabian Data
+
+# ## PN
+
+# Kerrdisk fit
+
+# In[3]:
+
+
+Image(filename="Final_figs/fabian_pn_kerrdisk_v1.jpg")
+
+
+# Relxill fit
+
+# In[5]:
+
+
+Image(filename="Final_figs/fabian_pn_relxill.jpg")
+
+
+# ## MOS
+
+# kerrdisk fit
+
+# In[7]:
+
+
+Image(filename="Final_figs/fabian_mos_kerrdisk_v2.jpg")
+
+
+# relxill fit
+
+# In[9]:
+
+
+Image(filename="Final_figs/fabian_mos_relxill.jpg")
+
+
+# # Giorgio Data
+
+# ## PN
+
+# laor2 fit
+
+# In[10]:
+
+
+Image(filename="Final_figs/giorgio_pn_laor2_v1.jpg")
+
+
+# kerrdisk fit
+
+# In[11]:
+
+
+Image(filename="Final_figs/giorgio_pn_kerrdisk_v1.jpg")
+
+
+# ## MOS
+
+# Laor2 fit
+
+# In[12]:
+
+
+Image(filename="Final_figs/giorgio_mos_laor2_v1.jpg")
+
+
+# relxill fit
+
+# In[14]:
+
+
+Image(filename="Final_figs/giorgio_mos_relxill.jpg")
+
+
+# 
